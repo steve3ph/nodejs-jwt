0fb3aa11803a37520c2edf8db7315c006061395a
steve3ph <steve3phtech@gmail.com>
2024-01-01 15:36:36 +0530

    vercel GIT

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
 const cookieParser = require('cookie-parser');
 const { requireAuth, checkUser } = require('./middleware/authMiddleware');
+require('dotenv').config();
+
 
 const app = express();
 
@@ -14,11 +16,26 @@ app.use(cookieParser());
 // view engine
 app.set('view engine', 'ejs');
 
-// database connection mongodb+srv://shaun:test1234@cluster0.del96.mongodb.net/node-auth
-const dbURI = 'mongodb+srv://steve3phtech:steve3phtech@clusterjwt.fuonk1r.mongodb.net/node-auth';
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
-  .then((result) => app.listen(3000))
-  .catch((err) => console.log(err));
+// database 
+// const dbURI = 'mongodb+srv://steve3phtech:steve3phtech@clusterjwt.fuonk1r.mongodb.net/node-auth';
+// mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+//   .then((result) => app.listen(3000))
+//   .catch((err) => console.log(err));
+mongoose.connect(process.env.DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+})
+.then((result) => {
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+})
+.catch((err) => {
+  console.error('Error connecting to MongoDB:', err);
+});
+
 
 // routes
 app.get('*', checkUser)
